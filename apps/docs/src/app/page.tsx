8da324b7b90be01cf88c66cdb232d85adafc7660
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-06-21 15:23:53 -0300

    firs commit

--- apps/docs/src/app/page.tsx
+++ apps/docs/src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
 
 function Gradient({
   conic,
@@ -124,14 +123,6 @@ export default function Page(): JSX.Element {
             </svg>
           </div>
         </div>
-      </div>
-
-      <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {LINKS.map(({ title, href, description }) => (
-          <Card href={href} key={title} title={title}>
-            {description}
-          </Card>
-        ))}
       </div>
     </main>
   );
