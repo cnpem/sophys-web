40da8f31b064b6fffd65f3f2c386d76a27e5005f
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-06-24 17:49:24 -0300

    spu: add basic navbar

---
+++ apps/spu-ui/src/app/_components/navbar.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@repo/ui";
+
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/queues", label: "Queues" },
+  { href: "/tray", label: "Tray" },
+  { href: "/data", label: "Data" },
+];
+
+export default function Navbar() {
+  const pathname = usePathname();
+  return (
+    <nav className="flex top-0 flex-row sticky w-full p-4 gap-2 border-b shadow-sm opacity-90 z-10">
+      {links.map(({ href, label }) => (
+        <Link
+          className={cn("text-lg", {
+            "text-blue-500 font-semibold": pathname === href,
+            "text-gray-500": pathname !== href,
+            "hover:text-blue-500": pathname !== href,
+          })}
+          href={href}
+          key={href}
+        >
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
+}
--- apps/spu-ui/src/app/page.tsx
+++ apps/spu-ui/src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Page(): JSX.Element {
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="flex flex-col items-center my-auto *:space-y-12">
         <ClickMe />
-
         <Input placeholder="Enter your email" type="email" />
       </div>
     </main>
