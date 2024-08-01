3acf5c51a250d3ae8f3410c54eb0a8b7cc94e98e
Matheus Luís <matheus.bernardi@lnls.br>
2024-08-01 11:34:05 -0300

    lint

--- packages/ui/src/sonner.tsx
+++ packages/ui/src/sonner.tsx
@@ -5,13 +5,13 @@ import { Toaster as Sonner, toast } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
-const Toaster = ({ ...props }: ToasterProps) => {
+function Toaster({ ...props }: ToasterProps) {
   const { theme = "system" } = useTheme();
 
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      theme={theme as ToasterProps["theme"]}
       toastOptions={{
         classNames: {
           toast:
@@ -26,6 +26,6 @@ const Toaster = ({ ...props }: ToasterProps) => {
       {...props}
     />
   );
-};
+}
 
 export { Toaster, toast };
