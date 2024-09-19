ebe017d903ed15e5634afa2a5714e09c582108f3
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-09-19 15:30:51 -0300

    turning off eslint rules that flag ui components added directly from shadcn

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ["@sophys-web/eslint-config/react.js"],
+  rules: {
+    "import/order": "off",
+    "@typescript-eslint/consistent-type-imports": "off",
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "react/jsx-sort-props": "off",
+  },
 };
