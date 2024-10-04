16861893cbe840ed089cf077d55fac1a1af6df89
Matheus Luís <matheus.bernardi@lnls.br>
2024-10-04 16:17:59 -0300

    add user avatar

---
+++ apps/spu-ui/src/app/_components/user-avatar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { LogOutIcon, UserIcon } from "lucide-react";
+import { auth, signOut } from "@sophys-web/auth";
+import { Button, buttonVariants } from "@sophys-web/ui/button";
+
+export default async function UserAvatar() {
+  const session = await auth();
+  if (!session) {
+    return (
+      <Link
+        className={buttonVariants({ variant: "link" })}
+        href="/api/auth/signin"
+      >
+        Sign in
+      </Link>
+    );
+  }
+  return (
+    <div className="flex items-center">
+      <UserIcon className="h-4 w-4" />
+      <span className="ml-2 mr-2">{session.user.name}</span>
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button size="icon" type="submit" variant="ghost">
+          <LogOutIcon className="text-primary h-4 w-4" />
+        </Button>
+      </form>
+    </div>
+  );
+}
