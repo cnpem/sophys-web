bf47fe8c08694f2ffef58269b33ca315b4b885fe
Matheus Luís <matheus.bernardi@lnls.br>
2024-10-10 15:35:49 -0300

    (fix): tailwind configs
    tailwind base config was not correctly configured to include shadcn's presets

--- apps/spu-ui/tailwind.config.ts
+++ apps/spu-ui/tailwind.config.ts
@@ -1,11 +1,9 @@
-// tailwind config is required for editor support
-
 import type { Config } from "tailwindcss";
-import sharedConfig from "@sophys-web/tailwind-config";
+import baseConfig from "@sophys-web/tailwind-config";
 
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./src/app/**/*.tsx"],
-  presets: [sharedConfig],
-};
-
-export default config;
+export default {
+  // We need to append the path to the UI package to the content array so that
+  // those classes are included correctly.
+  content: [...baseConfig.content, "../../packages/ui/src/*.{ts,tsx}"],
+  presets: [baseConfig],
+} satisfies Config;
