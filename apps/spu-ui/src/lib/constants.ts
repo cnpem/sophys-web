cd4563428eadb5c1275ef76779ae675a0c9362f4
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-10-28 17:39:44 -0300

    apps/spu-ui: updating consts and schemas based on plan defs
    updating ui variables related to the load_and_acquire plan parameters

--- apps/spu-ui/src/lib/constants.ts
+++ apps/spu-ui/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const trayRows = [
+export const trayColumns = [
   "01",
   "02",
   "03",
@@ -9,5 +9,11 @@ export const trayRows = [
   "08",
   "09",
   "10",
+  "11",
+  "12",
 ] as const;
-export const trayColumns = ["A", "B", "C", "D", "E", "F", "G", "H"] as const;
+export const trayRows = ["A", "B", "C", "D", "E", "F", "G", "H"] as const;
+export const trayOptions = ["Tray1", "Tray2"] as const;
+export const acquireTimeOptions = [
+  200, 100, 50, 25, 12.5, 6.25, 3.125, 1.5625, 0.5,
+] as const;
