5c9fb4e3afeedb7c8ebafc65779ff6acc7b1b837
Matheus Luís <matheus.bernardi@lnls.br>
2024-11-05 16:27:37 -0300

    add start pipeline and adjust some next configs

--- apps/spu-ui/next.config.js
+++ apps/spu-ui/next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  output: "standalone",
+  /** enable standalone output for docker self-hosting */
+  // output: "standalone",
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: ["@sophys-web/ui", "@sophys-web/api", "@sophys-web/auth"],
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  /** base path setup for reverse proxy */
+  basePath: "/spu-ui",
 };
