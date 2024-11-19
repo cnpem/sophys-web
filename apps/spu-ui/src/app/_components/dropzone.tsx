be8c89ccb8754519d15c5b61f46280999b31409c
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-11-19 15:02:45 -0300

    apps/spu-ui: animation element for pointing to the drop area

--- apps/spu-ui/src/app/_components/dropzone.tsx
+++ apps/spu-ui/src/app/_components/dropzone.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useDroppable } from "@dnd-kit/core";
+import { ArrowBigDownDashIcon } from "lucide-react";
 import { cn } from "@sophys-web/ui";
 
 interface DropzoneProps {
@@ -16,15 +17,17 @@ export function Dropzone(props: DropzoneProps) {
   return (
     <div
       className={cn(
-        "relative mb-8 flex h-fit w-full items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-4 font-medium",
+        "relative flex h-fit w-full items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-4 font-medium",
         isOver && "border-rose-600 bg-rose-600/10",
       )}
       ref={setNodeRef}
     >
       {props.children}
-      <p className="absolute -bottom-8 mt-2 text-center text-sm text-gray-500">
-        {isOver ? "Drop here" : "Drag and drop here"}
-      </p>
+      {isOver ? (
+        <div className="absolute bottom-1/2 animate-bounce rounded-full bg-rose-600/20 p-2 shadow-md">
+          <ArrowBigDownDashIcon className="h-6 w-6  text-rose-800" />
+        </div>
+      ) : null}
     </div>
   );
 }
