b1130014c48b3d25fbfeb67742cca0a7925f9239
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-11-21 09:26:27 -0300

    packages/api: queue item add batch schemas and route

--- packages/api/src/schemas/item.ts
+++ packages/api/src/schemas/item.ts
@@ -47,6 +47,63 @@ const addResponse = z.discriminatedUnion("success", [
   }),
 ]);
 
+const addBatchSubmit = z
+  .object({
+    items: z.array(
+      z.object({
+        name: z.string(),
+        args: z.array(z.any()),
+        kwargs: z.record(z.any()),
+        itemType: z.string(),
+      }),
+    ),
+  })
+  .transform((data) => {
+    return {
+      items: data.items.map(({ itemType, ...unchanged }) => ({
+        item_type: itemType,
+        ...unchanged,
+      })),
+    };
+  });
+
+const addBatchResponse = z
+  .object({
+    success: z.literal(true),
+    msg: z.string(),
+    qsize: z.number(),
+    items: z.array(
+      z.object({
+        name: z.string(),
+        args: z.array(z.any()),
+        kwargs: z.record(z.any()),
+        item_type: z.string(),
+        user: z.string(),
+        user_group: z.string(),
+        item_uid: z.string(),
+      }),
+    ),
+    results: z.array(
+      z.object({
+        success: z.literal(true),
+        msg: z.string(),
+      }),
+    ),
+  })
+  .transform((data) => {
+    return {
+      ...data,
+      items: data.items.map(
+        ({ item_type, user_group, item_uid, ...unchanged }) => ({
+          itemType: item_type,
+          userGroup: user_group,
+          itemUid: item_uid,
+          ...unchanged,
+        }),
+      ),
+    };
+  });
+
 const removeSubmit = z.object({
   uid: z.string(),
 });
@@ -55,4 +112,6 @@ export default {
   addSubmit,
   addResponse,
   removeSubmit,
+  addBatchSubmit,
+  addBatchResponse,
 };
