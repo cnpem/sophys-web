a385b551699264bf445bfed9df56a6d25a85146c
Matheus Luís <matheus.bernardi@lnls.br>
2024-11-26 08:33:40 -0300

    refactor dropzone
    to fit its content width

--- apps/spu-ui/src/app/_components/dropzone.tsx
+++ apps/spu-ui/src/app/_components/dropzone.tsx
@@ -17,14 +17,14 @@ export function Dropzone(props: DropzoneProps) {
   return (
     <div
       className={cn(
-        "relative flex h-fit w-full items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-4 font-medium",
+        "relative w-fit rounded-lg border-2 border-dashed border-muted p-2",
         isOver && "border-rose-600 bg-rose-600/10",
       )}
       ref={setNodeRef}
     >
       {props.children}
       {isOver ? (
-        <div className="absolute bottom-1/2 animate-bounce rounded-full bg-rose-600/20 p-2 shadow-md">
+        <div className="absolute bottom-1/2 left-1/2 animate-bounce rounded-full bg-rose-600/20 p-2 shadow-md">
           <ArrowBigDownDashIcon className="h-6 w-6  text-rose-800" />
         </div>
       ) : null}
