659652dd9385c8be011303eef53f8dbc8a4cd58a
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-12-04 16:52:38 -0300

    packages/ui: add sidebar component

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -2,6 +2,23 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@sophys-web/tailwind-config";
 
 export default {
-  content: [...baseConfig.content],
+    darkMode: ["class"],
+    content: [...baseConfig.content],
   presets: [baseConfig],
+    theme: {
+    	extend: {
+    		colors: {
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
+    		}
+    	}
+    }
 } satisfies Config;
