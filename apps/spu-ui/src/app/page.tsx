98376f7b62530ba9ee61f517b5d17bf50ecab63e
Matheus Luís <matheus.bernardi@lnls.br>
2024-12-06 10:55:03 -0300

    chore(spu-ui): remove queues page
    move experiment to main page through auth layer, for now

--- apps/spu-ui/src/app/page.tsx
+++ apps/spu-ui/src/app/page.tsx
@@ -1,13 +1,23 @@
+import { auth } from "@sophys-web/auth";
+import Experiment from "./_components/experiment";
 import UserAvatar from "./_components/user-avatar";
+import { getSamples } from "./actions/samples";
 
-export default function Page() {
-  return (
-    <main className="flex flex-col items-center gap-4 p-24">
-      <h1 className="text-4xl font-bold text-primary">Sophys Sapucaia UI</h1>
-      <p className="text-lg">
-        This is the UI for experiments conducted at the Sapucaia beamline.
-      </p>
-      <UserAvatar />
-    </main>
-  );
+export default async function Page() {
+  const session = await auth();
+  const samples = await getSamples();
+
+  if (!session || session.error) {
+    return (
+      <main className="flex flex-col items-center gap-4 p-24">
+        <h1 className="text-4xl font-bold text-primary">Sophys Sapucaia UI</h1>
+        <p className="text-lg">
+          This is the UI for experiments conducted at the Sapucaia beamline.
+        </p>
+        <UserAvatar />
+      </main>
+    );
+  }
+
+  return <Experiment initialSamples={samples} />;
 }
