27ff7a7b1443702a4b4695fe7de5f4f2a4ebc83c
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-12-11 14:19:59 -0300

    packages/api: fixing queue/get schema
    - removing optionals for all keys and using a union with an empty object form handling the empty object response.

--- packages/api/src/schemas/queue.ts
+++ packages/api/src/schemas/queue.ts
@@ -26,31 +26,31 @@ const getResponseSchema = z.object({
         .nullable(),
     }),
   ),
-  runningItem: z
-    .object({
-      name: z.string().optional(),
-      args: z.array(z.any()).optional(),
+  runningItem: z.union([
+    z.record(z.never()),
+    z.object({
+      name: z.string(),
+      args: z.array(z.any()).optional().nullable(),
       kwargs: z.record(z.any()).optional().nullable(),
-      itemType: z.string().optional(),
-      user: z.string().optional(),
-      userGroup: z.string().optional(),
-      itemUid: z.string().optional(),
-      properties: z.object({}).passthrough().optional(),
+      itemType: z.string(),
+      user: z.string(),
+      userGroup: z.string(),
+      itemUid: z.string().uuid(),
       result: z
         .object({
           exitStatus: z.string().optional(),
-          runUids: z.array(z.string()).optional(),
-          scanIds: z.array(z.string()).optional(),
-          timeStart: z.number().optional(),
-          timeStop: z.number().optional(),
+          runUids: z.array(z.string()),
+          scanIds: z.array(z.string()),
+          timeStart: z.number(),
+          timeStop: z.number(),
           msg: z.string().optional(),
           traceback: z.string().optional(),
         })
         .optional()
         .nullable(),
-    })
-    .optional()
-    .nullable(),
+      properties: z.object({}).passthrough().optional(),
+    }),
+  ]),
   planQueueUid: z.string().uuid(),
 });
 
