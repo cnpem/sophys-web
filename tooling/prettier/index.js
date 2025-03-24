ec5f9d2c1f4c1d5d3fa6832e40c1a23b6c54f147
Matheus Luís <matheus.bernardi@lnls.br>
2025-03-24 18:33:38 -0300

    chore: add Prettier formatting scripts and configuration across multiple packages

--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -1,3 +1,5 @@
+import { fileURLToPath } from "node:url";
+
 /** @typedef {import("prettier").Config} PrettierConfig */
 /** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
 /** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
@@ -8,6 +10,10 @@ const config = {
     "@ianvs/prettier-plugin-sort-imports",
     "prettier-plugin-tailwindcss",
   ],
+  tailwindConfig: fileURLToPath(
+    new URL("../../tooling/tailwind/tailwind.config.ts", import.meta.url),
+  ),
+  tailwindFunctions: ["cn", "cva"],
   importOrder: [
     "<TYPES>",
     "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
@@ -20,6 +26,22 @@ const config = {
     "^~/",
     "^[../]",
     "^[./]",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderTypeScriptVersion: "4.4.0",
+  overrides: [
+    {
+      files: "*.json.hbs",
+      options: {
+        parser: "json",
+      },
+    },
+    {
+      files: "*.js.hbs",
+      options: {
+        parser: "babel",
+      },
+    },
   ],
 };
 
