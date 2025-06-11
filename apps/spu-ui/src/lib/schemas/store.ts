e9836a80de0544c02ac26203b07abb486051b6c7
Bruno Carlos <bruno.carlos@lnls.br>
2025-06-11 18:55:14 -0300

    tmp: add arrays of string and number to the store example schema

--- apps/spu-ui/src/lib/schemas/store.ts
+++ apps/spu-ui/src/lib/schemas/store.ts
@@ -4,7 +4,6 @@ export const schema = z.object({
   name: z.string(),
   age: z.coerce.number().int().min(0).max(120),
   email: z.string().email(),
-  //   listOfStrings: z.array(z.string()),
-  //   listOfNumbers: z.array(z.number().int()),
-  //   lastUpdated: z.coerce.date(),
+  connectedUserNames: z.array(z.string()),
+  someNumbers: z.array(z.number()),
 });
