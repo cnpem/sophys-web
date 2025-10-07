a4bebfb5b8a00722aaf384fea431631457a41570
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2025-10-07 17:31:55 -0300

    feat(apps/qua-ui): refactor running item to render an alert when the queue stops showing details of the last item

---
+++ apps/qua-ui/src/app/_components/dashboard/running-item.tsx
@@ -0,0 +1,135 @@
+import { useEffect, useRef, useState } from "react";
+import { JsonEditor, monoLightTheme } from "json-edit-react";
+import { AlertCircleIcon } from "lucide-react";
+import { useQueue } from "@sophys-web/api-client/hooks";
+import { api } from "@sophys-web/api-client/react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@sophys-web/ui/alert-dialog";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@sophys-web/ui/card";
+import { HistoryItemContent } from "@sophys-web/widgets/history-item-utils";
+
+export function RunningItem() {
+  const { queue } = useQueue();
+  const runningItemUidRef = useRef<string | null>(null);
+  const [finishedItemUid, setFinishedItemUid] = useState<string | null>(null);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const status = api.status.get.useQuery(undefined, {
+    refetchInterval: 1 * 1000, // 1 second
+    refetchOnWindowFocus: "always",
+    refetchOnMount: "always",
+  });
+  const { data: historyLastItem, isPending } = api.history.get.useQuery(
+    // filter data.items for the item with itemUid matching runningItemUidRef.current
+    undefined,
+    {
+      enabled: !!finishedItemUid,
+      select: (data) =>
+        data.items.find((item) => item.itemUid === finishedItemUid),
+    },
+  );
+
+  useEffect(() => {
+    const prevRunningItemUid = runningItemUidRef.current;
+    const currentRunningItemUid = status.data?.runningItemUid;
+    const prevExists = prevRunningItemUid && prevRunningItemUid !== "";
+    const nowEmpty = !currentRunningItemUid;
+
+    if (prevExists && nowEmpty) {
+      setDialogOpen(true);
+      setFinishedItemUid(prevRunningItemUid);
+    }
+    runningItemUidRef.current = currentRunningItemUid ?? null;
+  }, [status.data?.runningItemUid]);
+
+  const runningItemData = queue.data?.runningItem;
+
+  // component state when a plan is currently running, showing the running item details
+  const isRunning = !!runningItemData && !!status.data?.runningItemUid;
+  // component state when a plan has just finished, triggering the dialog with last item details
+  const JustFinished = !runningItemData && !!finishedItemUid;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Running Item</CardTitle>
+        <CardDescription>Currently running task</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <AlertDialog open={dialogOpen} onOpenChange={setDialogOpen}>
+          {isRunning && (
+            <div className="space-y-4">
+              <div>
+                <h3 className="text-lg font-semibold">
+                  {runningItemData.name.replace(/_/g, " ")}
+                </h3>
+                <p className="text-muted-foreground">{runningItemData.user}</p>
+              </div>
+              {runningItemData.kwargs && (
+                <div>
+                  <h3>Parameters</h3>
+                  <JsonEditor
+                    restrictAdd={true}
+                    restrictDelete={true}
+                    restrictEdit={true}
+                    restrictDrag={true}
+                    data={runningItemData.kwargs}
+                    rootName={"kwargs"}
+                    theme={monoLightTheme}
+                    className="!text-sm"
+                  />
+                </div>
+              )}
+            </div>
+          )}
+          {!!JustFinished && (
+            <div className="flex flex-col items-center justify-center py-8 text-center">
+              <AlertCircleIcon className="text-muted-foreground mb-2 size-10" />
+              <p className="text-muted-foreground">No task currently running</p>
+            </div>
+          )}
+          {!isRunning && !JustFinished && (
+            <div className="flex flex-col items-center justify-center py-8 text-center">
+              <AlertCircleIcon className="text-muted-foreground mb-2 size-8" />
+              <p className="text-muted-foreground">No task currently running</p>
+            </div>
+          )}
+          <AlertDialogContent className="max-w-lg">
+            <AlertDialogHeader>
+              <AlertDialogTitle>Plan Finished</AlertDialogTitle>
+              <AlertDialogDescription>
+                The last running plan has finished. You can review the results
+                below.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            {isPending && <div>Loading last item...</div>}
+            {!isPending && !historyLastItem && (
+              <div className="mt-4">
+                <span>Last item not found.</span>
+                <span>Searching for itemUid: {finishedItemUid}</span>
+              </div>
+            )}
+            {!isPending && historyLastItem && (
+              <HistoryItemContent item={historyLastItem} />
+            )}
+            <AlertDialogFooter className="mt-0">
+              <AlertDialogAction>Close</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </CardContent>
+    </Card>
+  );
+}
