df3b8c8c1656cc5ada4599a680f725b35ad5a3e5
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2025-10-14 17:12:28 -0300

    feat(packages/api): update queue schema to treat empty object as null for running item response
    This prevents bugs and extra checks when conditionally rendering this data.

--- packages/api/src/schemas/queue.ts
+++ packages/api/src/schemas/queue.ts
@@ -1,5 +1,28 @@
 import { z } from "zod";
 
+const runningItemObjectSchema = z.object({
+  name: z.string(),
+  args: z.array(z.any()).optional().nullable(),
+  kwargs: z.record(z.any()).optional().nullable(),
+  itemType: z.string(),
+  user: z.string(),
+  userGroup: z.string(),
+  itemUid: z.string().uuid(),
+  result: z
+    .object({
+      exitStatus: z.string().optional(),
+      runUids: z.array(z.string()).optional(),
+      scanIds: z.array(z.coerce.number()).optional(),
+      timeStart: z.number(),
+      timeStop: z.number(),
+      msg: z.string().optional(),
+      traceback: z.string().optional(),
+    })
+    .optional()
+    .nullable(),
+  properties: z.object({}).passthrough().optional(),
+});
+
 const getResponseSchema = z.object({
   success: z.boolean(),
   msg: z.string(),
@@ -26,31 +49,18 @@ const getResponseSchema = z.object({
         .nullable(),
     }),
   ),
-  runningItem: z.union([
-    z.record(z.never()),
-    z.object({
-      name: z.string(),
-      args: z.array(z.any()).optional().nullable(),
-      kwargs: z.record(z.any()).optional().nullable(),
-      itemType: z.string(),
-      user: z.string(),
-      userGroup: z.string(),
-      itemUid: z.string().uuid(),
-      result: z
-        .object({
-          exitStatus: z.string().optional(),
-          runUids: z.array(z.string()).optional(),
-          scanIds: z.array(z.coerce.number()).optional(),
-          timeStart: z.number(),
-          timeStop: z.number(),
-          msg: z.string().optional(),
-          traceback: z.string().optional(),
-        })
-        .optional()
-        .nullable(),
-      properties: z.object({}).passthrough().optional(),
-    }),
-  ]),
+  runningItem: z.preprocess((val) => {
+    // If the input value is an empty object, change it to null.
+    if (
+      typeof val === "object" &&
+      val !== null &&
+      Object.keys(val).length === 0
+    ) {
+      return null;
+    }
+    // Otherwise, pass it through as is for the next validation step.
+    return val;
+  }, runningItemObjectSchema.nullable()), // Actual schema valitation
   planQueueUid: z.string().uuid(),
 });
 
