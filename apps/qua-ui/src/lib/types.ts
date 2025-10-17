e786cef36a67551b953deaa8d9fd6dbcd12647ca
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2025-10-17 17:54:12 -0300

    feat(packages/api): update queue schema to treat empty object as null for running item response and updating QueueItemProps type on apps.
    This prevents bugs and extra checks when conditionally rendering this data.

--- apps/qua-ui/src/lib/types.ts
+++ apps/qua-ui/src/lib/types.ts
@@ -4,9 +4,7 @@ import type { schemas } from "@sophys-web/api";
 export type ItemSubmit = z.infer<typeof schemas.item.addSubmit>;
 
 type QueueResponse = z.infer<typeof schemas.queue.getResponseSchema>;
-export type QueueItemProps =
-  | QueueResponse["items"][number]
-  | QueueResponse["runningItem"];
+export type QueueItemProps = QueueResponse["items"][number];
 
 type HistoryResponse = z.infer<typeof schemas.history.getResponseSchema>;
 export type HistoryItemProps = HistoryResponse["items"][number];
