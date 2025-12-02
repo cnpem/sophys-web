68b56b60454959b50f9010caf0e9c62f931bd960
igor.torquato@lnls.br <igor.torquato@lnls.br>
2025-12-02 10:43:17 -0300

    fix: add numbers as word boundaries for device regex match
    Since only letters were consider for word boundaries, cases with
    numbers would fail to parse. Like: m2_bender would become m2bender
    instead of m2_bender

--- packages/api/src/utils.ts
+++ packages/api/src/utils.ts
@@ -46,5 +46,5 @@ const snakeToCamelFetcher = async ({
 export const zodSnakeFetcher = createZodFetcher(snakeToCamelFetcher);
 
 export function camelToSnakeCase(str: string) {
-  return str.replace(/([a-z])([A-Z])/g, "$1_$2").toLowerCase();
+  return str.replace(/([a-z0-9])([A-Z])/g, "$1_$2").toLowerCase();
 }
