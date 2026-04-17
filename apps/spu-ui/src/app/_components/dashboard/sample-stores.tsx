1b703652153fe381136a27c2abfeefe7dd39e841
Bruno Carlos <bruno.carlos@lnls.br>
2026-04-17 09:22:36 -0300

    feat(apps/spu-ui): add SampleStores component to manage sample store selection for setup 1 and 2 and resize dashboard columns width based on the store width.

---
+++ apps/spu-ui/src/app/_components/dashboard/sample-stores.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { GlassWaterIcon } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@sophys-web/ui/select";
+import {
+  WindowCard,
+  WindowCardAction,
+  WindowCardContent,
+  WindowCardHeader,
+  WindowCardTitle,
+} from "@sophys-web/ui/window-card";
+import { Samples as Setup1Samples } from "../store/setup1/sample-store";
+import { Samples as Setup2Samples } from "../store/setup2/sample-store";
+
+export function SampleStores() {
+  const [selectedStore, setSelectedStore] = useState<"setup1" | "setup2">(
+    "setup1",
+  );
+  return (
+    <WindowCard className="w-fit">
+      <WindowCardHeader>
+        <WindowCardTitle>
+          <GlassWaterIcon className="mx-1 size-4" />
+          Samples
+        </WindowCardTitle>
+        <WindowCardAction>
+          <Select
+            value={selectedStore}
+            onValueChange={(value) =>
+              setSelectedStore(value as "setup1" | "setup2")
+            }
+          >
+            <SelectTrigger className="mb-0.5 bg-white">
+              <SelectValue placeholder="Select Sample Store" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="setup1">Setup 1</SelectItem>
+              <SelectItem value="setup2">Setup 2</SelectItem>
+            </SelectContent>
+          </Select>
+        </WindowCardAction>
+      </WindowCardHeader>
+      <WindowCardContent>
+        {selectedStore === "setup1" && <Setup1Samples />}
+        {selectedStore === "setup2" && <Setup2Samples />}
+      </WindowCardContent>
+    </WindowCard>
+  );
+}
