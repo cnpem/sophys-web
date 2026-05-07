7c72b6a75e98b4b4c15c8b3c3a0bea72a97bb76e
Bruno Carlos <bruno.carlos@lnls.br>
2026-05-07 12:28:09 -0300

    refactor(qua-ui): fix columns order in the CustomQueueTable component using defaultColumnsMap for column definitions

--- apps/qua-ui/src/app/_components/queue/custom-queue.tsx
+++ apps/qua-ui/src/app/_components/queue/custom-queue.tsx
@@ -1,14 +1,22 @@
 import type { QueueTableColumns } from "@sophys-web/widgets/queue-table/columns";
-import { columnHelper, columns } from "@sophys-web/widgets/queue-table/columns";
+import {
+  columnHelper,
+  defaultColumnsMap,
+} from "@sophys-web/widgets/queue-table/columns";
 import { DataTable as QueueTable } from "@sophys-web/widgets/queue-table/data-table";
 import { CustomEditItemDialog } from "./custom-edit-item";
 
 const customColumns = [
-  ...columns.filter((col) => col.id !== "edit"),
+  defaultColumnsMap.drag,
+  defaultColumnsMap.name,
+  defaultColumnsMap.user,
+  defaultColumnsMap.params,
   columnHelper.display({
     id: "edit",
     cell: ({ row }) => <CustomEditItemDialog item={row.original} />,
   }),
+  defaultColumnsMap.copy,
+  defaultColumnsMap.delete,
 ] as QueueTableColumns;
 
 export function CustomQueueTable() {
