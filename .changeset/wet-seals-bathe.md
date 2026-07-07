739f2d848ff3a7f8713cd299af74048dd0650153
Bruno Carlos <bruno.carlos@lnls.br>
2026-07-07 18:24:10 -0300

    fix(packages/tiled-client): Fixing the schema for the container GET response

---
+++ .changeset/wet-seals-bathe.md
@@ -0,0 +1,5 @@
+---
+"@sophys-web/tiled-client": patch
+---
+
+Fixing the schema for the container GET response for the first level of the container response as an object with contents and metadata keys, where the values are unknown to match the actual response from the backend.
--- packages/tiled-client/src/lib/schemas/container-get.ts
+++ packages/tiled-client/src/lib/schemas/container-get.ts
@@ -15,24 +15,17 @@ const FullContainerMetadataAndDataApiV1ContainerFullPathGetQueryParams =
     filename: z.union([z.string(), z.null()]).optional(),
   });
 
-const FullContainerMetadataAndDataApiV1ContainerFullPathGetResponse = z.object({
-  data: z.union([z.unknown(), z.null()]),
-  error: z
-    .union([
-      z.object({
-        code: z.number(),
-        message: z.string(),
-      }),
-      z.null(),
-    ])
-    .optional(),
-  links: z.union([z.unknown(), z.null()]).optional(),
-  meta: z.union([z.unknown(), z.null()]).optional(),
-});
-
-export const containerGetResponse =
-  FullContainerMetadataAndDataApiV1ContainerFullPathGetResponse;
 export const containerGetParams =
   FullContainerMetadataAndDataApiV1ContainerFullPathGetParams;
 export const containerGetQueryParams =
   FullContainerMetadataAndDataApiV1ContainerFullPathGetQueryParams;
+
+const containerNode = z.object({
+  contents: z.record(z.string(), z.unknown()),
+  metadata: z.record(z.string(), z.unknown()),
+});
+
+/**
+ * Schema for the response for the GET request for the first level of a container, containing the contents and metadata keys and unknown values.
+ */
+export const containerGetResponse = containerNode;
