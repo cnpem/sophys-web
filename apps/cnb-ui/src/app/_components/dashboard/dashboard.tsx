04485d549e117e6f1252c5bed781baadabe6c0fa
Murilo Theodoro Moreira Santos <murilo.moreira@lnls.br>
2026-09-22 14:49:46 -0300

    feat: creat a card to show beamline status

--- apps/cnb-ui/src/app/_components/dashboard/dashboard.tsx
+++ apps/cnb-ui/src/app/_components/dashboard/dashboard.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import { useQueue } from "@sophys-web/api-client/hooks";
-import { cn } from "@sophys-web/ui";
 import { ButtonGroup } from "@sophys-web/ui/button-group";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@sophys-web/ui/card";
+import { ItemGroup } from "@sophys-web/ui/item";
 import { ScrollArea } from "@sophys-web/ui/scroll-area";
 import { CompactQueue } from "@sophys-web/widgets/compact-queue";
 import { Console } from "@sophys-web/widgets/console";
@@ -11,7 +17,10 @@ import {
   EnvironmentControls,
   QueueControls,
 } from "@sophys-web/widgets/control-bar/control-bar";
+import { MachineInfo } from "./machine";
+import { Monochromator4CMEnergy } from "./monochromator4cm";
 import { OnDemandQueueItems } from "./on-demand-queue-items";
+import { VPU } from "./vpu";
 
 export function Dashboard() {
   return (
@@ -25,13 +34,29 @@ export function Dashboard() {
           <OnDemandQueueItems />
         </ButtonGroup>
       </ControlBar>
-      <div className="flex gap-2 p-2 pt-16 sm:flex-col lg:flex-row">
-        <ScrollArea className="w-full lg:h-svh lg:min-w-2/3">
-          <div className="flex flex-col gap-2">
-            <CompactQueue />
+      <div className="flex gap-3 p-8 pt-10 pb-10 sm:flex-col lg:min-h-screen lg:flex-row">
+        <ScrollArea className="h-full w-full lg:min-w-2/3">
+          <CompactQueue />
+        </ScrollArea>
+
+        <div className="flex h-full flex-col gap-2 lg:w-1/2">
+          <Card className="gap-2 pb-2">
+            <CardHeader>
+              <CardTitle>Beamline Status</CardTitle>
+              <CardDescription>Current beamline conditions</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <ItemGroup className="grid grid-cols-2 items-center">
+                <Monochromator4CMEnergy />
+                <MachineInfo />
+                <VPU />
+              </ItemGroup>
+            </CardContent>
+          </Card>
+          <div className="flex h-62">
             <Console />
           </div>
-        </ScrollArea>
+        </div>
       </div>
     </>
   );
