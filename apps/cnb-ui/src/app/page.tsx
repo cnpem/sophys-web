0e0e000f52280b2bed9a24d55ec75157e8fba23f
Murilo Theodoro Moreira Santos <murilo.moreira@lnls.br>
2026-09-22 14:48:32 -0300

    feat: add CNB logo to show in login page

--- apps/cnb-ui/src/app/page.tsx
+++ apps/cnb-ui/src/app/page.tsx
@@ -1,19 +1,31 @@
+import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { api, HydrateClient } from "@sophys-web/api-client/server";
 import { auth } from "@sophys-web/auth";
 import { buttonVariants } from "@sophys-web/ui/button";
+import { appendBasePath } from "~/lib/appendBasePath";
 import { Dashboard } from "./_components/dashboard/dashboard";
+import { env as runtimeEnv } from "node:process";
+import { PVWSConnectionHandler } from "@sophys-web/pvws-store";
+
 
 export default async function Page() {
+  const pvwsUrl = runtimeEnv.PVWS_URL;
   const session = await auth();
 
   if (!session) {
     return (
-      <main className="flex flex-col items-center gap-4 p-24">
-        <h1 className="text-primary text-4xl font-bold">cnb-ui</h1>
+      <main className="flex flex-col items-center gap-10 p-40">
+        <Image
+          src={appendBasePath("/logo_cnb.png")}
+          alt="Logo"
+          width={200}
+          height={200}
+        />
+        <h1 className="text-primary text-4xl font-bold">Sophys CNB</h1>
         <p className="text-lg">
-          This is the UI for experiments conducted at the cnb-ui beamline.
+          This is the UI for experiments conducted at the Carnaúba beamline.
         </p>
         <Link
           className={buttonVariants({ variant: "link" })}
@@ -38,6 +50,7 @@ export default async function Page() {
   return (
     <HydrateClient>
       <Dashboard />
+      <PVWSConnectionHandler url={pvwsUrl} />
     </HydrateClient>
   );
 }
